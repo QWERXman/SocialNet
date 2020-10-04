e9ab66c277f19407d56a279520a16bb5a84b9b8c
QWERXman <yablokov-e@mail.ru>
2020-10-04 17:39:07 +0300

    Добавил билд в гит, потому что на хостинге не хватает мощности собрать его

---
+++ client/build/precache-manifest.bc6b9b4566b5031bf5c83de469c71081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce33eff7d0ae43d77f48287ff28a5e3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "824ef760c6e38a3fc845",
+    "url": "/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "815ff1b40912025cbd3f",
+    "url": "/static/css/main.7beb5dba.chunk.css"
+  },
+  {
+    "revision": "824ef760c6e38a3fc845",
+    "url": "/static/js/2.f995ef9b.chunk.js"
+  },
+  {
+    "revision": "815ff1b40912025cbd3f",
+    "url": "/static/js/main.91dd0af6.chunk.js"
+  },
+  {
+    "revision": "72c8ea0ef25b294ebc23",
+    "url": "/static/js/runtime-main.ea895c90.js"
+  }
+]);
